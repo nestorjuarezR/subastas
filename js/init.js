9b007a1090970f95b7f3c1d0a12a117fece693bf
Nestor Juarez <nesk.company@gmail.com>
2022-06-18 19:44:01 -0500

    Correccion de bugs en responsivo y datepicker

--- js/init.js
+++ js/init.js
@@ -3,11 +3,18 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-    $(".dropdown-trigger").dropdown();
+    $(".dropdown-trigger").dropdown({
+        hover: false,
+        coverTrigger: false,
+        alignment: 'center',
+        constrainWidth: true
+    });
     $('.datepicker').datepicker({
       selectYears: 90,
-    format: 'yyyy-mm-dd'
+      format: 'yyyy-mm-dd',
+      yearRange: [1910, 2022]
   });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
